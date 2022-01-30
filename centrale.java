8e201dd93e4d97192c64d820a16cac6d202d6eda
Calixte <41114634+Cali-xte@users.noreply.github.com>
2022-01-30 09:40:01 +0100

    Nettoyage de messageArrived + commentaires

--- centrale.java
+++ centrale.java
@@ -19,8 +19,8 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 public class centrale implements MqttCallback {
 private static ArrayList<String[]> valeurs = new ArrayList<>();
 MqttClient client;
-Tableau tableau= new Tableau(); //On crée un Objet Tableau qui permet de gérer le stockage
-csv obj_csv = new csv(); // Permet d'utiliser la classe csv
+Tableau tableau= new Tableau();                                 //On crée un Objet Tableau qui permet de gérer le stockage
+csv obj_csv = new csv();                                        // Permet d'utiliser la classe csv
 
 public void centrale() {
 }
@@ -31,22 +31,21 @@ public static void main(String[] args) {
 
 public void doDemo() {
     try {
-        String uri = "tcp://calixte.ovh:1883"; // Renseignement de l'adresse du broker
+        String uri = "tcp://calixte.ovh:1883";                  // Renseignement de l'adresse du broker
         String clientID = UUID.randomUUID().toString();
         MemoryPersistence persistence = new MemoryPersistence();
         System.out.println("*** uri = "+uri);
         System.out.println("*** UUID = "+clientID);
-        client = new MqttClient(uri, clientID, persistence); // Permet de créer une entité client que nous allons pouvoir connecter par la suite.
+        client = new MqttClient(uri, clientID, persistence);    // Permet de créer une entité client que nous allons pouvoir connecter par la suite.
 
-        client.connect(); // Connection au broker
+        client.connect();                                       // Connection au broker
         client.setCallback(this);
-        // La centrale demande aux capteurs déjà démarrés de s'annoncer à nouveau
-        MqttMessage centraleEnLigne = new MqttMessage();
+        MqttMessage centraleEnLigne = new MqttMessage();        // La centrale demande aux capteurs déjà démarrés de s'annoncer à nouveau
         String demarrage = "disponible";
         centraleEnLigne.setPayload(demarrage.getBytes());
-        client.subscribe("annonce"); // S'abonne au cannal annonce
-        client.subscribe("stop"); // S'abonne au cannal stop
-        client.publish("annonce", centraleEnLigne); // Publie sur le cannal annonce et un message MQTT
+        client.subscribe("annonce");                            // S'abonne au cannal annonce
+        client.subscribe("stop");                               // S'abonne au cannal stop
+        client.publish("annonce", centraleEnLigne);             // Publie sur le cannal annonce et un message MQTT
     } catch (MqttException e) {
         e.printStackTrace();
     }
@@ -58,35 +57,35 @@ public void connectionLost(Throwable cause) {
 }
 
 @Override
-public void messageArrived(String topic, MqttMessage message) throws Exception { // Method pour recevoir les messages
+public void messageArrived(String topic, MqttMessage message) throws Exception { // Methode pour recevoir les messages
     System.out.println("["+topic+"] "+message);
     //Récupération de la date
     DateFormat format = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
     Calendar calendar = Calendar.getInstance();
     String date = String.valueOf(format.format(calendar.getTime()));
 
-    if (topic.toString().equals("annonce")&&!message.toString().equals("disponible")) {
-        client.subscribe(message.toString());
+    if (topic.toString().equals("annonce")&&!message.toString().equals("disponible")) { // Si un capteur s'annonce
+        client.subscribe(message.toString());                   // On s'abonne à son canal
         System.out.println(message+" OK");
     }
 
-    if (topic.toString().equals("stop")) { // lorsque qu'on reçoit un message sur le cannal stop
-        obj_csv.creation_csv(); // On appel la fonction qui permet de créer le csv dans la classe csv
+    if (topic.toString().equals("stop")) {                      // lorsque qu'on reçoit un message sur le cannal stop
+        obj_csv.creation_csv();                                 // On appelle la fonction qui permet de créer le csv dans la classe csv
         System.out.println("Fermeture de la centrale");
-        System.exit(0); // La centrale s'arrete
+        System.exit(0);                                         // La centrale s'arrete
     }
 
-    String canal = topic.toString().replaceAll("[^0-9]", "");
-    client.publish("afficheur"+canal, message);
-    String texte=this.tableau.totext(canal);                    //On récupére les valeurs des capteurs
-    MqttMessage historique = new MqttMessage();                 //On le transforme dans le bon format
-    historique.setPayload(texte.getBytes());                    
-    client.publish("historique"+canal, historique);             ////On les envoie dans le canal destiné aux afficheurs
-    if (message != null&&!topic.equals("annonce")){             //Si le message n'est pas null et qu'il ne fait pas parti des annonces:
-        String info = message.toString();                       //On transforme les variables en String
+    if (message != null&&!topic.equals("annonce")){             // Si le message n'est pas nul et qu'il ne fait pas partie des annonces:
+        String canal = topic.toString().replaceAll("[^0-9]", "");
+        client.publish("afficheur"+canal, message);
+        String texte=this.tableau.totext(canal);                // On récupére les valeurs des capteurs
+        MqttMessage historique = new MqttMessage();             // On le transforme dans le bon format
+        historique.setPayload(texte.getBytes());                    
+        client.publish("historique"+canal, historique);         // On les envoie dans le canal destiné aux afficheurs
+        String info = message.toString();                       // On transforme les variables en String
         String test= canal.toString();
-        this.tableau.trier(canal,info);                         //On envoie les informations pour pouvoir les stocker
-        obj_csv.objets(topic,message.toString(),date);          //Dès qu'un message est reçu, on créer un objet capteur avec numéro, valeur et date reçue via la classe csv
+        this.tableau.trier(canal,info);                         // On envoie les informations pour pouvoir les stocker
+        obj_csv.objets(topic,message.toString(),date);          // Dès qu'un message est reçu, on crée un objet capteur avec numéro, valeur et date reçue via la classe csv
     }
 }
 
